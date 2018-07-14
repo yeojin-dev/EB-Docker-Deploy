4121133642cb324b3bc618a6356a08dc78914e30
Yeojin Kim <yeojin.dev@gmail.com>
2018-07-14 15:18:15 +0900

    settings/production.py 기능 추가 : runserver 대응, S3와 RDS 연결
    runserver, AWS 배포에 따라 ALLOWED_HOST, LOG_DIR 변경하도록 코드 추가함.
    S3 버킷, RDS(postgreSQL) 연결 위한 코드 추가함.

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -1,30 +1,54 @@
+import subprocess
+import sys
+
 from .base import *
 
 DEBUG = False
 
 f = open(os.path.join(SECRETS_DIR, 'production.json'))
 json_object = json.load(f)
-ALLOWED_HOSTS.append(json_object['ALLOWED_HOSTS'])
 
-# Static
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(ROOT_DIR, '.media')
+ALLOWED_HOSTS += json_object['ALLOWED_HOSTS']
+
+RUNSERVER = 'runserver' in sys.argv
+if RUNSERVER:
+    DEBUG = True
+    ALLOWED_HOSTS = [
+        '127.0.0.1',
+        'localhost',
+    ]
+
 
 # WSGI
 WSGI_APPLICATION = 'config.wsgi.production.application'
 
+INSTALLED_APPS += [
+    'storages',
+]
+
+# DEFAULT_FILE_STORAGE = 'config.storages.S3DefaultStorage'
+# STATICFILES_STORAGE = 'config.storages.S3StaticStorage'
+
+AWS_ACCESS_KEY_ID = json_object['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = json_object['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = json_object['AWS_STORAGE_BUCKET_NAME']
+AWS_DEFAULT_ACL = json_object['AWS_DEFAULT_ACL']
+AWS_S3_REGION_NAME = json_object['AWS_S3_REGION_NAME']
+AWS_S3_SIGNATURE_VERSION = json_object['AWS_S3_SIGNATURE_VERSION']
+
 # DB
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = json_object['DATABASES']
 print(DATABASES)
 
-LOG_DIR = '/var/log/django'
+
+if os.path.exists('/var/log/django'):
+    LOG_DIR = '/var/log/django'
+else:
+    LOG_DIR = os.path.join(ROOT_DIR, '.log')
+    os.makedirs(LOG_DIR, exist_ok=True)
+
+subprocess.call(['chmod', '755', LOG_DIR])
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
